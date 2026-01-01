582df62b62796ea9fcc8fa89cb5d079ded6feb9a
epyuri <14885236+epyuri@user.noreply.gitee.com>
2026-01-01 22:35:06 +0800

    v 1.0

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ document.getElementById("lianXi").onclick = () => {
   alert(
       "- 作者 QQ 3412735994 -\n" +
       "- 交流群 1056969651 -\n" +
-      "我说怎么两天没人加我，原来我把qq号写错了\n"
+      "我说怎么两天没人加我, 原来我把qq号写错了, 现在是对的了, 技术交流打游戏啥的都可以加\n"
   );
 }
 document.getElementById("github").onclick = () => {
