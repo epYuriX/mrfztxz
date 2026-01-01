2112a0748a894fbd8941627cc875509d085349a7
epyuri <14885236+epyuri@user.noreply.gitee.com>
2026-01-01 22:34:06 +0800

    v 1.0

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ import "./js/console/main.js";
 
 document.getElementById("lianXi").onclick = () => {
   alert(
-      "- 作者 QQ 3412725994 -\n" +
-      "- 交流群 1056969651 -\n"
+      "- 作者 QQ 3412735994 -\n" +
+      "- 交流群 1056969651 -\n" +
+      "我说怎么两天没人加我，原来我把qq号写错了\n"
   );
 }
 document.getElementById("github").onclick = () => {
